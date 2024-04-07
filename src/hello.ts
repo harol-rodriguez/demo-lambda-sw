a036a16874d38b0790f8eb437a6f647e068ae55a
Harol <harol.rodriguez@arisale.com.pe>
2024-04-07 12:34:47 -0500

    :tada: init demo swapi

---
+++ src/hello.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import { v4 } from "uuid"
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const reqBody = JSON.parse(event.body as string)
+  console.log(event)
+  const jedy = {
+    ...reqBody,
+    id: v4()
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({hello: "SW DEMO! AUA!", e: event})
+  }
+}
