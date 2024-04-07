48f7a0793bca7fb26a7918c54fec8f4e9bedf788
Harol <harol.rodriguez@arisale.com.pe>
2024-04-07 13:52:06 -0500

    :white_check_mark: unit test

--- test/app/useCases/getModifiedData/GetModifiedDataUseCase.test.ts
+++ test/app/useCases/getModifiedData/GetModifiedDataUseCase.test.ts
@@ -3,25 +3,61 @@ import { GetModifiedDataUseCase } from "../../../../src/app/useCases/getModified
 
 describe('GetModifiedDataUseCase', () => {
   it('should return mapped data', async () => {
-    // Crea una instancia del caso de uso con un mock de ExternalApiService
     const externalApiServiceMock = {
       fetchDataFromExternalApi: jest.fn(() => ({
-        results: [
-          // Datos de ejemplo aquí...
-        ],
+        results: [{
+            "name": "CR90 corvette", 
+            "model": "CR90 corvette", 
+            "manufacturer": "Corellian Engineering Corporation", 
+            "cost_in_credits": "3500000", 
+            "length": "150", 
+            "max_atmosphering_speed": "950", 
+            "crew": "30-165", 
+            "passengers": "600", 
+            "cargo_capacity": "3000000", 
+            "consumables": "1 year", 
+            "hyperdrive_rating": "2.0", 
+            "MGLT": "60", 
+            "starship_class": "corvette", 
+            "pilots": [], 
+            "films": [
+                "https://swapi.py4e.com/api/films/1/", 
+                "https://swapi.py4e.com/api/films/3/", 
+                "https://swapi.py4e.com/api/films/6/"
+            ], 
+            "created": "2014-12-10T14:20:33.369000Z", 
+            "edited": "2014-12-20T21:23:49.867000Z", 
+            "url": "https://swapi.py4e.com/api/starships/2/"
+        },],
       })),
     };
     const useCase = new GetModifiedDataUseCase(externalApiServiceMock as unknown as ExternalApiService);
 
-    // Ejecuta el caso de uso
     const modifiedData = await useCase.execute();
 
-    // Verifica que el método del servicio externo se haya llamado
     expect(externalApiServiceMock.fetchDataFromExternalApi).toHaveBeenCalled();
 
-    // Verifica que los datos mapeados sean correctos (puedes agregar más expectativas según tus necesidades)
-    expect(modifiedData).toEqual([
-      // Datos mapeados aquí...
-    ]);
+    expect(modifiedData).toEqual([{
+      "nombre": "CR90 corvette",
+      "modelo": "CR90 corvette",
+      "fabricante": "Corellian Engineering Corporation",
+      "costo_en_creditos": "3500000",
+      "longitud": "150",
+      "velocidad_maxima_atmosfera": "950",
+      "tripulacion": "30-165",
+      "pasajeros": "600",
+      "capacidad_carga": "3000000",
+      "consumibles": "1 year",
+      "clasificacion_nave": "corvette",
+      "pilotos": [],
+      "peliculas": [
+          "https://swapi.py4e.com/api/films/1/",
+          "https://swapi.py4e.com/api/films/3/",
+          "https://swapi.py4e.com/api/films/6/"
+      ],
+      "creado": "2014-12-10T14:20:33.369000Z",
+      "editado": "2014-12-20T21:23:49.867000Z",
+      "url": "https://swapi.py4e.com/api/starships/2/"
+    }]);
   });
 });
